bug fix: not set keepdim=True when calculation gradient of b to avoid inconsistent dimension(layers.py)

assignment2/cs231n/classifiers/fc_net.py
@@ -45,9 +45,13 @@ class TwoLayerNet(object):
     # weights and biases using the keys 'W1' and 'b1' and second layer weights #
     # and biases using the keys 'W2' and 'b2'.                                 #
     ############################################################################
-    self.params['W1'] = weight_scale * np.random.randn(input_dim, hidden_dim)
+    #self.params['W1'] = weight_scale * np.random.randn(input_dim, hidden_dim)
+    #self.params['b1'] = np.zeros(hidden_dim)
+    #self.params['W2'] = weight_scale * np.random.randn(hidden_dim, num_classes)
+    #self.params['b2'] = np.zeros(num_classes)
+    self.params['W1'] = np.random.normal(0, weight_scale, (input_dim, hidden_dim))
     self.params['b1'] = np.zeros(hidden_dim)
-    self.params['W2'] = weight_scale * np.random.randn(hidden_dim, num_classes)
+    self.params['W2'] = np.random.normal(0, weight_scale, (hidden_dim, num_classes))
     self.params['b2'] = np.zeros(num_classes)
     ############################################################################
     #                             END OF YOUR CODE                             #
@@ -110,7 +114,6 @@ class TwoLayerNet(object):
     grads['W2'] = dW2
     grads['b2'] = db2
     dx, dW1, db1 = affine_relu_backward(dhidden_layer,cache_layer1)
-    dW1 = dW1.reshape(W1.shape)
     dW1 += reg * W1
     grads['W1'] = dW1
     grads['b1'] = db1

assignment2/cs231n/layers.py
@@ -26,10 +26,12 @@ def affine_forward(x, w, b):
   #############################################################################
   # D : (2,120)
   # w : (120,3)
+   
   reshapeX = np.zeros((x.shape[0] , np.prod(x.shape)/x.shape[0]))            
   for i in xrange(x.shape[0]):  
     #reshapeX[i] = np.ravel(x[i])
     reshapeX[i] = x[i].reshape(-1)
+  
   out = np.dot(reshapeX,w) + b
   #############################################################################
   #                             END OF YOUR CODE                              #
@@ -62,8 +64,8 @@ def affine_backward(dout, cache):
   
   #dx = dout.dot(w.T)
   dx = np.dot(dout,w.T) 
-  #dx = np.reshape(dx,x.shape)
-  dx = dx.reshape(x.shape)
+  dx = np.reshape(dx,x.shape)
+  #dx = dx.reshape(x.shape)
  
   #(D,N).(N,M) = (D,M) 
   reshapeX = np.zeros((x.shape[0] , np.prod(x.shape)/x.shape[0]))            
@@ -72,7 +74,7 @@ def affine_backward(dout, cache):
     reshapeX[i] = x[i].reshape(-1)
   #dw = (reshapeX).T.dot(dout)
   dw = np.dot(reshapeX.T,dout)
-  db = np.sum(dout, axis=0,keepdims=True)
+  db = np.sum(dout, axis=0)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -117,8 +119,9 @@ def relu_backward(dout, cache):
   #############################################################################
   # TODO: Implement the ReLU backward pass.                                   #
   #############################################################################
-  dout[x <= 0] =0
-  dx = dout   
+  #dout[x <= 0] =0
+  #dx = dout
+  dx = ( x > 0 ) * dout
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################